f4b4557e15b00dfa3ea6a0a4a9e077c0ad4ca6a2
Mayan <mshoshan@uci.edu>
2020-01-15 21:57:50 -0800

    add game

---
+++ blockAR/Assets/scripts/buttons.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+public class buttons : MonoBehaviour
+{
+
+    public enum TestEnum { set, delete, color1, color2, color3};
+
+    //This is what you need to show in the inspector.
+    GameObject manager;
+    public TestEnum choices;
+    void Awake()
+    {
+        manager = GameObject.Find("/origin");
+    }
+
+
+    void Start()
+    {
+        EventTrigger trigger = GetComponent<EventTrigger>();
+        EventTrigger.Entry entry = new EventTrigger.Entry();
+        entry.eventID = EventTriggerType.PointerDown;
+        entry.callback.AddListener((data) => { OnPointerDownDelegate((PointerEventData)data); });
+        trigger.triggers.Add(entry);
+    }
+
+    public void OnPointerDownDelegate(PointerEventData data)
+    {
+        Debug.Log("press down");
+        switch (choices)
+        {
+            case TestEnum.set:
+                Debug.Log("set");
+                manager.GetComponent<managerAR>().Place();
+                break;
+            case TestEnum.delete:
+                Debug.Log("delete");
+                manager.GetComponent<managerAR>().delete();
+                break;
+            case TestEnum.color1:
+                manager.GetComponent<managerAR>().color1();
+                break;
+            case TestEnum.color2:
+                manager.GetComponent<managerAR>().color2();
+                break;
+            case TestEnum.color3:
+                manager.GetComponent<managerAR>().color3();
+                break;
+            default:
+                Debug.Log("NOTHING");
+                break;
+        }
+
+    }
+
+}
---
+++ blockAR/Assets/scripts/managerAR.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.UI;
+
+using System;
+
+public class managerAR : MonoBehaviour
+{
+    public Camera arCamera;
+    GameObject ObjectToPlace;
+    public GameObject obj1;
+    public GameObject obj2;
+    public GameObject obj3;
+    public GameObject PlaceIndicator;
+    private ARSessionOrigin aROrigin;
+    private ARRaycastManager m_RaycastManager;
+    private Pose placementPose;
+    public Text t;
+    private bool placementIsValid = false;
+    GameObject lookedAt;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ObjectToPlace = obj1;
+        aROrigin = GetComponent<ARSessionOrigin>();
+        m_RaycastManager = GetComponent<ARRaycastManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (lookedAt == null) { t.text = "no object found"; }
+        else { t.text = lookedAt.name; }
+        UpdatePlacementPose();
+        UpdatePlacementIndicator();
+     
+    }
+
+    public void color1() { ObjectToPlace = obj1; }
+    public void color2() { ObjectToPlace = obj2; }
+    public void color3() { ObjectToPlace = obj3; }
+
+    public void Place() {
+
+        if (placementIsValid) {
+
+            if (lookedAt == null)
+            {
+                Instantiate(ObjectToPlace, placementPose.position, placementPose.rotation);
+            }
+            else if (lookedAt.name == "indicator") { Instantiate(ObjectToPlace, placementPose.position, placementPose.rotation); }
+            else
+            {
+
+                Vector3 tempPos = new Vector3(placementPose.position.x, (placementPose.position.y) + 0.2f, placementPose.position.z);
+                Instantiate(ObjectToPlace, tempPos, placementPose.rotation);
+
+            }
+
+
+    }
+
+
+    }
+
+    public void delete()
+    {
+
+        if (placementIsValid && lookedAt != null) {
+            if (lookedAt.name != "indicator")
+            {
+                Destroy(lookedAt);
+                lookedAt = null;
+            }
+        }
+
+
+    }
+
+
+
+        private void UpdatePlacementPose()
+    {
+      
+       
+        var screenCenter = Camera.main.ViewportToScreenPoint(new Vector3(0.5f, 0.5f));
+        var hits = new List<ARRaycastHit>();
+        m_RaycastManager.Raycast(screenCenter, hits, TrackableType.Planes);
+        Ray ray = Camera.main.ScreenPointToRay(screenCenter);
+        RaycastHit hitObject;
+        if (Physics.Raycast(ray, out hitObject))
+        {
+            GameObject placementObject = hitObject.transform.gameObject;
+            if (placementObject != null)
+            {
+                lookedAt = placementObject;
+            }
+            else { lookedAt = null; }
+        }
+        else { lookedAt = null; }
+        placementIsValid = hits.Count > 0;
+        if (placementIsValid)
+        {
+            placementPose = hits[0].pose;
+            var camForward = Camera.current.transform.forward;
+            Vector3 cameraBearing = new Vector3(camForward.x, 0, camForward.z).normalized;
+            placementPose.rotation = Quaternion.LookRotation(cameraBearing);
+
+        }
+
+
+    }
+
+
+
+
+
+    private void UpdatePlacementIndicator()
+    {
+        if (placementIsValid)
+        {
+            PlaceIndicator.SetActive(true);
+            PlaceIndicator.transform.SetPositionAndRotation(placementPose.position, placementPose.rotation);
+        }
+        else {
+            PlaceIndicator.SetActive(false);
+
+        }
+    }
+}
